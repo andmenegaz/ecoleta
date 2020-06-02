5aeef645af717210e728f89f1dc1ad55c23fbdd3
andmenegaz <andre.menegaz@litma.com.br>
2020-06-02 09:25:41 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ecoleta | Next Level Week | RocketSeat
---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/users', () => {
+    console.log('Listagem de Usuários!')
+})
+
+
+app.listen(3333)
